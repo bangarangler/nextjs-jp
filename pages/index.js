a99ed066f572030f4f34e40a7bf6e51c95074d8d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-16 11:31:06 -0400

    Old School LifeCycle Methods

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,37 @@ import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout.js";
 
 class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "I am Index Page"
+    };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  updateTitle() {
+    this.setState({ title: "updated index page" });
+  }
+
   render() {
+    console.log("render");
     return (
       <BaseLayout>
         <h1>Welcome Page!</h1>
+        <h2>{this.state.title}</h2>
+        <button onClick={() => this.updateTitle()}>Change Title</button>
       </BaseLayout>
     );
   }
