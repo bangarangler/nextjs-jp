5f6076d42601afbf13814d3779139ab4979ca21d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-16 10:08:48 -0400

    Added About page with nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-const Index = () => {
-  return (
-    <div>
-      <h1>Welcome Page!</h1>
-    </div>
-  );
-};
+import React from "react";
+
+//const Index = () => {
+//return (
+//<div>
+//<h1>Welcome Page!</h1>
+//</div>
+//);
+//};
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Page!</h1>
+      </div>
+    );
+  }
+}
 
 export default Index;
