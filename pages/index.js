7b10f8b27f4c35a081f9a39f2d1e9f17f3e3a02e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-17 09:06:27 -0400

    getInitialProps ... magic happens

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import BaseLayout from "../components/layouts/BaseLayout.js";
 import SuperComponent from "../components/SuperComponent.js";
 
 class Index extends SuperComponent {
+  static getInitialProps() {
+    console.log('get initial props')
+    return {initialData: [1,2,3,4]}
+  }
   constructor(props) {
     super(props);
     this.state = {
@@ -23,18 +27,19 @@ class Index extends SuperComponent {
     console.log("componentWillUnmount");
   }
 
-  updateTitle() {
+  updateTitle = () => {
     this.setState({ title: "updated index page" });
   }
 
   render() {
     const { title } = this.state;
+    const initialData = this.props.initialData;
     console.log("render");
     return (
       <BaseLayout>
         <h1>Welcome Page!</h1>
         <h2>{title}</h2>
-        <button onClick={() => this.updateTitle()}>Change Title</button>
+        <button onClick={this.updateTitle}>Change Title</button>
       </BaseLayout>
     );
   }
