bde08628e7fa5774ab5723463ac3576a884e9f95
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-20 18:50:20 -0400

    small styling tweaks reactstrap

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -1,36 +1,86 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import {Button, FormGroup, Label } from 'reactstrap';
+
+const validateInputs = validate => {
+  let errors = {};
+  //if (!values.email) {
+  //errors.email = 'Required';
+  //} else if (
+  //!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  //) {
+  //errors.email = 'Invalid email address';
+  //}
+  return errors;
+};
+
+const INITIAL_VALUES = {
+  title: '',
+  company: '',
+  location: '',
+  position: '',
+  description: '',
+  startDate: '',
+  endDate: '',
+};
 
 const PortfolioCreateForm = () => (
   <div>
     <Formik
-      initialValues={{ email: '', description: '' }}
-      validate={values => {
-        let errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
+      initialValues={INITIAL_VALUES}
+      validate={validateInputs}
+      onSubmit={(values, {setSubmitting}) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
           setSubmitting(false);
         }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
+      }}>
+      {({isSubmitting}) => (
         <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="text" name="description" />
-          <ErrorMessage name="description" component="div" />
+          <FormGroup>
+            <Label>Title</Label>
+            <Field className="form-control" type="text" name="title" />
+            <ErrorMessage name="title" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Company</Label>
+            <Field className="form-control" type="text" name="company" />
+            <ErrorMessage name="company" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Location</Label>
+            <Field className="form-control" type="text" name="location" />
+            <ErrorMessage name="location" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Position</Label>
+            <Field className="form-control" type="text" name="position" />
+            <ErrorMessage name="position" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Description</Label>
+            <Field className="form-control" type="textarea" name="description" component="textarea" />
+            <ErrorMessage name="description" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Start Date</Label>
+            <Field className="form-control" type="text" name="startDate" />
+            <ErrorMessage name="startDate" component="div" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>End Date</Label>
+            <Field className="form-control" type="text" name="endDate" />
+            <ErrorMessage name="endDate" component="div" />
+          </FormGroup>
+
           <button type="submit" disabled={isSubmitting}>
-            Submit
+            Create
           </button>
         </Form>
       )}
@@ -42,47 +92,46 @@ export default PortfolioCreateForm;
 //import React from 'react';
 
 //export default class PortfolioCreateForm extends React.Component {
-  //constructor(props) {
-    //super(props)
-    //this.state = {title: "", description: "", language: ""}
+//constructor(props) {
+//super(props)
+//this.state = {title: "", description: "", language: ""}
 
-    //this.handleChange = this.handleChange.bind(this)
-    //this.handleSubmit = this.handleSubmit.bind(this)
-  //}
-
-  //handleChange(event) {
-    //const field = event.target.name
-    //this.setState({[field]: event.target.value})
-  //}
-
-  //handleSubmit(event) {
-    //alert('A name was submitted:' + this.state.title + " " + this.state.description + " " + this.state.language)
-    //event.preventDefault()
-  //}
-
-  //render() {
-    //return (
-      //<form onSubmit={this.handleSubmit}>
-      //<label>
-        //Name:
-        //<input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
-      //</label>
-      //<label>
-        //Description:
-        //<textarea name="description" value={this.state.description} onChange={this.handleChange} />
-      //</label>
-      //<label>
-        //Pick your favorite Programming Language
-      //<select name="language" value={this.state.language} onChange={this.handleChange}>
-        //<option value="javascript">JavaScript</option>
-      //<option value="java">Java</option>
-      //<option value="c">C</option>
-      //<option value="python">Python</option>
-      //</select>
-      //</label>
-      //<input type="submit" value="submit" />
-      //</form>
-    //)
-  //}
+//this.handleChange = this.handleChange.bind(this)
+//this.handleSubmit = this.handleSubmit.bind(this)
 //}
 
+//handleChange(event) {
+//const field = event.target.name
+//this.setState({[field]: event.target.value})
+//}
+
+//handleSubmit(event) {
+//alert('A name was submitted:' + this.state.title + " " + this.state.description + " " + this.state.language)
+//event.preventDefault()
+//}
+
+//render() {
+//return (
+//<form onSubmit={this.handleSubmit}>
+//<label>
+//Name:
+//<input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
+//</label>
+//<label>
+//Description:
+//<textarea name="description" value={this.state.description} onChange={this.handleChange} />
+//</label>
+//<label>
+//Pick your favorite Programming Language
+//<select name="language" value={this.state.language} onChange={this.handleChange}>
+//<option value="javascript">JavaScript</option>
+//<option value="java">Java</option>
+//<option value="c">C</option>
+//<option value="python">Python</option>
+//</select>
+//</label>
+//<input type="submit" value="submit" />
+//</form>
+//)
+//}
+//}
--- pages/portfolioNew.js
+++ pages/portfolioNew.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout.js';
 import BasePage from '../components/BasePage.js';
-import  withAuth  from '../components/hoc/withAuth.js';
+import withAuth from '../components/hoc/withAuth.js';
 import PortfolioCreateForm from '../components/portfolios/PortfolioCreateForm.js';
+import {Row, Col} from 'reactstrap';
 
 class PortfolioNew extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage className="portfolio-create-page" title="Create New Portfolio">
-      <PortfolioCreateForm />
+        <BasePage
+          className="portfolio-create-page"
+          title="Create New Portfolio">
+          <Row>
+            <Col md="6">
+              <PortfolioCreateForm />
+            </Col>
+          </Row>
         </BasePage>
       </BaseLayout>
     );
