6d186580df8f8fdb83e6cfa550b6529fed63360c
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-20 16:08:40 -0400

    Updated some reactstrap styles

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -4,6 +4,7 @@ import BasePage from '../components/BasePage.js';
 import axios from 'axios';
 import Portfolio from './portfolio/[id].js';
 import Link from 'next/link';
+import { Row, Col, Card, CardHeader, CardBody, CardText, CardTitle } from 'reactstrap';
 
 class Portfolios extends React.Component {
   static async getInitialProps() {
@@ -20,15 +21,23 @@ class Portfolios extends React.Component {
   }
 
   renderPosts(posts) {
-    return posts.map(post => {
+    return posts.map(( post, index ) => {
       return (
-        <div key={post.id}>
-          <li>
-            <Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        </div>
+          <Col md="4">
+          <React.Fragment key={index}>
+            <span>
+              <Card className="portfolio-card">
+                <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                <CardBody>
+                  <p className="portfolio-card-city"> Some Location {index} </p>
+                  <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
+                  <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                  <div className="readMore"> </div>
+                </CardBody>
+              </Card>
+            </span>
+          </React.Fragment>
+        </Col>
       );
     });
   }
@@ -37,8 +46,8 @@ class Portfolios extends React.Component {
     const {posts} = this.props;
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="Portfolios Page">
-          <ul>{this.renderPosts(posts)}</ul>
+        <BasePage className="portfolio-page" title="Portfolios Page">
+          <Row>{this.renderPosts(posts)}</Row>
         </BasePage>
       </BaseLayout>
     );
