14464503fc138994afdee5bc4ef423192cb8a7f6
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-24 15:48:26 -0400

    Added mark up and styling for blogs section..
    still need to tweak some.

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../shared/Header.js';
+import Head from 'next/head';
 
 const BaseLayout = props => {
   const {className, children, isAuthenticated, user} = props;
   const headerType = props.headerType || 'default';
   return (
+    <>
+    <Head>
+    <title>Jonathan Dain Palacio</title>
+    <script src="https://kit.fontawesome.com/530582d694.js"></script>
+    </Head>
     <div className="layout-container">
       <Header
         className={`port-nav-${headerType}`}
@@ -15,6 +21,7 @@ const BaseLayout = props => {
         <div className="wrapper">{children}</div>
       </main>
     </div>
+    </>
   );
 };
 
