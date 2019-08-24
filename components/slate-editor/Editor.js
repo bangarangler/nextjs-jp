115dc672ef36f02ac09fd2a89c18a6c1d08ff0fb
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-24 17:57:01 -0400

    Continuing to set up slate / react-slate

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -23,6 +23,14 @@ const initialValue = Value.fromJSON({
   },
 });
 
+function CodeNode(props) {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
 export default class SlateEditor extends React.Component {
   state = {
     value: initialValue,
@@ -37,12 +45,35 @@ export default class SlateEditor extends React.Component {
     this.setState({value});
   };
 
+  onKeyDown = (e, editor, next) => {
+    if (e.key != 'x' || !e.ctrlKey) return next()
+    e.preventDefault()
+    const isCode = editor.value.blocks.some(block => block.type === 'code')
+    editor.setBlocks(isCode ? 'paragraph' : 'code')
+  };
+
+  renderNode = (props, editor, next) => {
+    switch (props.node.type) {
+      case 'code':
+        return <CodeNode {...props} />
+      case 'paragraph':
+        return <p {...props.attributes}>{props.children}</p>
+      default:
+        return next()
+    }
+  }
+
   render() {
     const {isLoaded} = this.state;
     return (
       <>
         {isLoaded && (
-          <Editor value={this.state.value} onChange={this.onChange} />
+          <Editor
+            value={this.state.value}
+            onChange={this.onChange}
+            onKeyDown={this.onKeyDown}
+            renderNode={this.renderNode}
+          />
         )}
       </>
     );
