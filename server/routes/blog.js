132cd75eeba4904068fa87ed92ae679645b77dcb
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-25 11:58:12 -0400

    Added Controller and Route for get...
    getBlogById. tested and working via postman

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const blogController = require('../controllers/blog.js');
 const authService = require('../services/auth.js');
 
+router.get('/:id', blogController.getBlogById);
+
 router.post(
   '',
   authService.checkJWT,
