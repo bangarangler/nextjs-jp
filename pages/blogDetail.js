30b0a4c31781365d8d5d036b9230821b424f3228
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-28 13:51:16 -0400

    Single page view for blog...
    in place and slightly styled

--- pages/blogDetail.js
+++ pages/blogDetail.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout.js';
 import BasePage from '../components/BasePage.js';
+import { getBlogBySlug } from '../actions';
+import { Row, Col } from 'reactstrap';
 
 class BlogDetail extends React.Component {
-  static async getInitialProps() {
-
+  static async getInitialProps({ query }) {
+    let blog = {}
+    const slug = query.slug;
+    try {
+      blog = await getBlogBySlug(slug)
+    } catch (err) {
+      console.error(err)
+    }
+    return { blog }
   }
   render() {
+    const { blog } = this.props;
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage className="blog-detail-page" title="Blog Detail Page!">
+        <BasePage className="blog-detail-page">
+      <Row>
+      <Col md={{ size: 8, offset: 2 }}>
+      <div dangerouslySetInnerHTML={{__html: blog.story}}></div>
+      </Col>
+      </Row>
         </BasePage>
       </BaseLayout>
     );
