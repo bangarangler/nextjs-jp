8a1f2eb907d46e06b19642d53c0f9c5550779633
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-29 19:41:41 -0400

    Added meta tags for SEO

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -9,6 +9,8 @@ const BaseLayout = props => {
     <>
       <Head>
         <title>{title}</title>
+          <meta name="description" content="Hi I'm Jon! I'm a web developer with a extreme interest in marketing and business! I've owned a few business's in my time and I still enjoy the thrill of planning and organizing a business. Regardless if it's day one or you have been in business for generations; I'm confident that I can make it even better!" />
+            <meta name="keywords" content="jon palacio portfolio, jon palacio developer, jon palacio web developer, jon palacio freelancing, SSR NextJS" />
         <script src="https://kit.fontawesome.com/530582d694.js" />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
