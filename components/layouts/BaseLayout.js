c691d1eba4d8966e1ae48e02ac0e7c5bc51a64fd
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-29 19:47:57 -0400

    Added meta tags for opengraph og:title, etc

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -3,14 +3,38 @@ import Header from '../shared/Header.js';
 import Head from 'next/head';
 
 const BaseLayout = props => {
-  const {className, children, isAuthenticated, user, isSiteOwner, title} = props;
+  const {
+    className,
+    children,
+    isAuthenticated,
+    user,
+    isSiteOwner,
+    title,
+  } = props;
   const headerType = props.headerType || 'default';
   return (
     <>
       <Head>
         <title>{title}</title>
-          <meta name="description" content="Hi I'm Jon! I'm a web developer with a extreme interest in marketing and business! I've owned a few business's in my time and I still enjoy the thrill of planning and organizing a business. Regardless if it's day one or you have been in business for generations; I'm confident that I can make it even better!" />
-            <meta name="keywords" content="jon palacio portfolio, jon palacio developer, jon palacio web developer, jon palacio freelancing, SSR NextJS" />
+        <meta
+          name="description"
+          content="Hi I'm Jon! I'm a web developer with a extreme interest in marketing and business! I've owned a few business's in my time and I still enjoy the thrill of planning and organizing a business. Regardless if it's day one or you have been in business for generations; I'm confident that I can make it even better!"
+        />
+        <meta
+          name="keywords"
+          content="jon palacio portfolio, jon palacio developer, jon palacio web developer, jon palacio freelancing, SSR NextJS"
+        />
+        <meta
+          property="og:title"
+          content="Jon Palacio - programmer, developer, full-stack"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="http://localhost:3000" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Hi I'm Jon! I'm a web developer with a extreme interest in marketing and business! I've owned a few business's in my time and I still enjoy the thrill of planning and organizing a business. Regardless if it's day one or you have been in business for generations; I'm confident that I can make it even better!"
+        />
         <script src="https://kit.fontawesome.com/530582d694.js" />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
