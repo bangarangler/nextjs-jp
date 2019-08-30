564874cd0e47261995250a237a762a7dcebeae12
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-30 11:28:39 -0400

    Setting up for heroku

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 // add values before deploy
 module.exports = {
-  DB_URI: process.env.DB_URI,
+  DB_URI: `mongodb+srv://jon:2bxme2RqsHdTustzi4@@portfolio-next-jp-prod-5lc18.mongodb.net/test?retryWrites=true&w=majority`,
   NAMESPACE: 'https://jp-portfolio-dain.herokuapp.com'
 }
 console.log(process.env.DB_URI)
