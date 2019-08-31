ae9bd4cdc7199a8cc22515cd767334db22d1c61e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 13:47:22 -0400

    Heroku down

--- .env-config.js
+++ .env-config.js
@@ -1,8 +1,8 @@
 const prod = process.env.NODE_ENV == 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://nextjs-jp.bangarangler.now.sh' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://nextjs-jp.bangarangler.now.sh',
+  'process.env.BASE_URL': prod ? 'https://jp-portfolio-dain.herokuapp.com' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://jp-portfolio-dain.herokuapp.com',
   'process.env.CLIENT_ID': 'tDvlaGtL20f3bhW0ujL7iu02KiOeucft'
 }
 //https://nextjs-jp.bangarangler.now.sh/
