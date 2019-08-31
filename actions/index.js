1c1b69518fd61a8858686f6cf4578277c55b8be8
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 15:38:12 -0400

    Testing

--- actions/index.js
+++ actions/index.js
@@ -5,7 +5,7 @@ import {getCookieFromReq} from '../helpers/utils.js';
 
 const axiosInstance = axios.create({
   baseURL: `${process.env.BASE_URL}/api/v1`,
-  timeout: 5000,
+  timeout: 8000,
 });
 
 const setAuthHeader = req => {
