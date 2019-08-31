25a56bfc51671796170eb67a72a0d0f6a7b7c56f
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 15:24:46 -0400

    Testing

--- actions/index.js
+++ actions/index.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 import {getCookieFromReq} from '../helpers/utils.js';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${process.env.BASE_URL}/api/v1`,
   timeout: 3000,
 });
 
--- server/services/auth.js
+++ server/services/auth.js
@@ -2,7 +2,8 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
-//const namespace = process.env.NAMESPACE;
+const config = require('../config');
+const NAMESPACE = config.NAMESPACE
 
 // MIDDLEWARE
 exports.checkJWT = jwt({
@@ -20,7 +21,7 @@ exports.checkJWT = jwt({
 exports.checkRole = role => (req, res, next) => {
   const user = req.user;
 
-  if (user && (user[`${process.env.NAMESPACE}/role`] === role)) {
+  if (user && user[NAMESPACE + 'role'] && (user[NAMESPACE + '/role'] === role)) {
     next();
   } else {
     return res
