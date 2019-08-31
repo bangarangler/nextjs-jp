39ab30c28cb5608c4d16a03d215bab9d98f86358
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 11:04:35 -0400

    auth0 confusion

--- .env-config.js
+++ .env-config.js
@@ -1,9 +1,9 @@
 const prod = process.env.NODE_ENV == 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://jp-portfolio-dain.herokuapp.com' : 'http://localhost:3000',
+  'process.env.BASE_URL': prod ? 'https://nextjs-jp.bangarangler.now.sh' : 'http://localhost:3000',
   'process.env.NAMESPACE': 'https://nextjs-jp.bangarangler.now.sh',
-  'process.env.CLIENT_ID': 'https://jp-portfolio-dain.herokuapp.com'
+  'process.env.CLIENT_ID': 'https://nextjs-jp.bangarangler.now.sh'
 }
 //https://nextjs-jp.bangarangler.now.sh/
 //https://jp-portfolio-dain.herokuapp.com
