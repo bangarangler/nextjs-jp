f735e50c8f8371680fd6f9946bbd1b136e05d91f
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 10:38:02 -0400

    now

--- .env-config.js
+++ .env-config.js
@@ -1,7 +1,9 @@
 const prod = process.env.NODE_ENV == 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://jp-portfolio-dain.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://jp-portfolio-dain.herokuapp.com',
+  'process.env.BASE_URL': prod ? 'https://nextjs-jp.bangarangler.now.sh' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://nextjs-jp.bangarangler.now.sh',
   'process.env.CLIENT_ID': 'tDvlaGtL20f3bhW0ujL7iu02KiOeucft'
 }
+//https://nextjs-jp.bangarangler.now.sh/
+//https://jp-portfolio-dain.herokuapp.com
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 // add values before deploy
 module.exports = {
-  DB_URI: `mongodb+srv://jon:2bxme2RqsHdTustzi4@@portfolio-next-jp-prod-5lc18.mongodb.net/test?retryWrites=true&w=majority`,
+  DB_URI: `mongodb+srv://i${process.env.MONGO_USER}:${process.env.MONGO_PW}@portfolio-next-jp-prod-5lc18.mongodb.net/test?retryWrites=true&w=majority`,
   NAMESPACE: 'https://jp-portfolio-dain.herokuapp.com'
 }
 
