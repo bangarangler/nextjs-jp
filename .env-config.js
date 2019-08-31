628fcad131d4bbfab67bda4123356c9f063a8676
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-31 11:13:30 -0400

    typo

--- .env-config.js
+++ .env-config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV == 'production';
 module.exports = {
   'process.env.BASE_URL': prod ? 'https://nextjs-jp.bangarangler.now.sh' : 'http://localhost:3000',
   'process.env.NAMESPACE': 'https://nextjs-jp.bangarangler.now.sh',
-  'process.env.CLIENT_ID': 'https://nextjs-jp.bangarangler.now.sh'
+  'process.env.CLIENT_ID': 'tDvlaGtL20f3bhW0ujL7iu02KiOeucft'
 }
 //https://nextjs-jp.bangarangler.now.sh/
 //https://jp-portfolio-dain.herokuapp.com
